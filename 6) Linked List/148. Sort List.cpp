bce3e5b7fde896f389f6b609071b122e13742131
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-05-31 16:27:40 +0530

    Update 148. Sort List.cpp

--- 6) Linked List/148. Sort List.cpp
+++ 6) Linked List/148. Sort List.cpp
@@ -10,45 +10,50 @@
  */
 class Solution {
 public:
-    static int count(ListNode* a){
-        int c = 0;
-        while(a){
-            c++;
-            a = a -> next;
-        }
-        return c;
-    }
-    static ListNode* BubbleSort(ListNode* head,int c){
-        if(head ==  NULL || head -> next == NULL)
+    static ListNode* getMiddle(ListNode* head){
+        if(head == NULL || head -> next == NULL)
             return head;
-        ListNode* prev = NULL;
-        auto f = head;
-        auto s = head -> next;
-        auto forw = head -> next -> next;
-        while(--c){
-            if(f-> val > s -> val){
-                    s -> next = f ;
-                    f-> next = forw; 
-                    if(prev != NULL)
-                        prev -> next= s;
-                    else
-                        head = s;
-                    auto temp = s;
-                    s = f;
-                    f = temp;
-            }
-            prev = f;
-            f = f -> next;
-            s = s -> next;
-            if(s)
-                forw = s -> next;
+        auto slow = head;
+        auto fast = head;
+        auto curr = head;
+        while(fast && fast -> next){
+            fast = fast -> next -> next;
+            curr = slow;
+            slow = slow -> next;
         }
-        return head;
+        return curr;
+    }
+    static ListNode* merge(ListNode* l1, ListNode* l2){
+        if(l1 == NULL)
+            return l2;
+        else if(l2 == NULL)
+            return l1;
+        auto curr = new ListNode();
+        auto temp = curr;
+        while(l1 && l2){
+            if(l1 -> val < l2 -> val)
+            {
+                temp -> next = new ListNode(l1 -> val);
+                temp = temp -> next;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                temp -> next = new ListNode(l2 -> val);
+                temp = temp -> next;
+                l2 = l2 -> next;
+            }
+        }
+        temp -> next = (l1 != NULL)? l1 : l2;
+        return curr -> next;
     }
     ListNode* sortList(ListNode* head) {
-        int c = count(head);
-        while(c--)
-            head = BubbleSort(head,c+1);
-        return head;
+        if(head == NULL || head -> next == NULL)
+            return head;
+        auto mid = getMiddle(head);
+        auto right = sortList(mid-> next);
+        mid -> next = NULL;
+        auto left = sortList(head);
+        return merge(left,right);
     }
 };
